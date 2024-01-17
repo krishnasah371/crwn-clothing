856704e505a0bccd97d5303ae958bbd3fd5105d0
Krishna Sah <shahkrishna918@gmail.com>
2024-01-17 14:00:22 -0600

    updated redux to redux saga

--- src/pages/contact/contact.component.jsx
+++ src/pages/contact/contact.component.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import './contact.styles.scss'
+import React from 'react';
+import './contact.styles.scss';
 
 const ContactPage = () => (
   <section className="contact-page">
     <h1 className="contact-header">CONTACT PAGE</h1>
+    <h2>
+      If you have any issues please contact:
+      <a href="https://sites.google.com/view/kpsah/home"> Contact me</a>
+    </h2>
   </section>
-)
+);
 
-export default ContactPage
+export default ContactPage;
